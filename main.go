e18244a3c3fcf702a3223939c4900e7b744a0a53
David Ko <david.ko@velvetreactor.com>
2019-04-06 02:07:21 -0400

    Add goroutines

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Starting the Bauss...")
+	nworkers := flag.Int("workers", 1, "number of worker threads")
+	flag.Parse()
+
+	msg := fmt.Sprintf("Starting the Bauss + %d worker(s)", *nworkers)
+	fmt.Println(msg)
+
+	for i := 0; i < *nworkers; i++ {
+		go worker()
+	}
+}
+
+func worker() {
+	for {
+	}
 }
